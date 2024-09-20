e524bbfe303741cf951c9bc6f048edca37671b31
Luiz Matheus <luiz.matheus@ufrpe.br>
2024-09-20 11:45:31 -0300

    getting project done

--- dp_knapsack.py
+++ dp_knapsack.py
@@ -14,51 +14,47 @@ class item:
         )
 
 
-def knapsack(capacity, weights, values, n):
-    # Criando a tabela dp com tamanho (n+1) x (capacity+1) 
+def knapsack(capacity, items, n):
+    # Criando a tabela dp com tamanho (n+1) x (capacity+1)
     dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
 
     # Preenchendo a tabela
-    for i in range(1, n + 1):
-        for w in range(1, capacity + 1):
+    for i in range(1, n + 1):  # itens (linha)
+        for w in range(1, capacity + 1):  # capacidades (coluna)
             # Se o peso do item atual é menor ou igual ao peso limite
-            if weights[i - 1] <= w:
+            if items[i - 1].weight <= w:
                 # Decisão: incluir o item ou não
                 dp[i][w] = max(
-                    dp[i - 1][w], values[i - 1] + dp[i - 1][w - weights[i - 1]]
+                    dp[i - 1][w], items[i - 1].value + dp[i - 1][w - items[i - 1].weight]
                 )
             else:
                 # Caso o item não possa ser incluído
                 dp[i][w] = dp[i - 1][w]
 
+            # LOGs
+            # print(f"Item atual: {items[i - 1].name}")
+            # print(f"i = {i}")
+            # print(f"Peso disponível = {w}")
+            # print(f"Valores dos itens inclusos = {dp[i][w]}\n")
+
+    print("Matriz Programação Dinâmica:")
+    for row in dp:
+        print(row)
     # O valor máximo será dp[n][capacity]
     return dp[n][capacity]
 
 
-item_1 = item(id=1, name="Smartphone", weight="1", value="6")
-item_2 = item(id=2, name="Mouse", weight="4", value="9")
-item_3 = item(id=3, name="Keyboard", weight="2", value="3")
-item_4 = item(id=4, name="Headphones", weight="3", value="8")
+# Criação e alocação dos itens na lista de itens
+item_1 = item(id=3, name="Pedra", weight=3, value=8)
+item_2 = item(id=2, name="Rocha", weight=2, value=3)
+item_3 = item(id=4, name="Prata", weight=4, value=9)
+item_4 = item(id=1, name="Brita", weight=1, value=6)
 
-items_list = []
-
-for i in range(1, 5):
-    items_list.append(globals()[f"item_{i}"])
-
+items_list = [item_1, item_2, item_3, item_4]
 
 # Exemplo de uso
-values = [8, 3, 9, 6]
-weights = [3, 2, 4, 1]
 capacity = 5
-n = len(values)
+n = len(items_list)
 
-max_value = knapsack(capacity, weights, values, n)
+max_value = knapsack(capacity, items_list, n)
 print("O valor máximo que pode ser obtido é:", max_value)
-
-dp = [[0 for _ in range(capacity + 1) for _ in range(n + 1)]]
-
-print("Visualizando linhas:")
-for linha in dp:
-    for elemento in linha:
-        print(elemento, end=" ")
-    print()
